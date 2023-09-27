5d94be9f67cbd87c49c756b04156a138e6c65f81
ilja <iljafff@mail.ru>
2023-09-27 20:00:33 +0300

    Написал програаму реализованную с помощью блок-схемы

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿string[] CreatArray(int size)
+{
+    string[] array = new string[size];  
+    for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Введите элемент массива ({i+1}/{size}) >: ");
+            array[i] = Console.ReadLine();
+        }
+    return array;
+}    
+
+void WriteArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+
+}
+
+string[] NewArray(string[] array)
+{
+    int n = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            n++;
+        }
+    }
+    string[] res = new string[n];
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            res[j] = array[i];
+            j++;
+        }
+    }
+    return res;
+}
+
+Console.Write("введите размер массива ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreatArray(num2);
+WriteArray(myArray);
+string[] res = NewArray(myArray);
+WriteArray(res);
